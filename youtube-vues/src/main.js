7d65c9fa525bbdb950dd9fc8fada70b3f68ccfef
Shahram Ghassemi <shahramg92@hotmail.com>
2018-11-06 16:33:00 -0600

    render content to DOM

--- youtube-vues/src/main.js
+++ youtube-vues/src/main.js
@@ -2,7 +2,5 @@ import Vue from 'vue';
 import App from './App';
 
 new Vue({
-  render: function(createElement) {
-    return createElement(App);
-  }
-});
+  render: h => h(App)
+}).$mount('#app');
