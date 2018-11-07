899b61d7069d3cc60cc08d6e982788ee2464a511
Shahram Ghassemi <shahramg92@hotmail.com>
2018-11-06 18:47:18 -0600

    install axios & get ajax request

--- youtube-vues/src/App.vue
+++ youtube-vues/src/App.vue
@@ -1,16 +1,30 @@
 <template>
   <div>
-    <SearchBar></SearchBar>
+    <SearchBar @termChange="onTermChange"></SearchBar>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
 import SearchBar from './components/SearchBar';
+const API_KEY = 'AIzaSyCdZvfs3-bxQ1pkC6aJIgmF21ViQTnJW8M';
 
 export default {
   name: 'App',
   components: {
     SearchBar
+  },
+  methods: {
+    onTermChange(searchTerm)  {
+      axios.get('https://www.googleapis.com/youtube/v3/search', {
+        params: {
+          key: API_KEY,
+          type: 'video',
+          part: 'snippet',
+          q: searchTerm
+        }
+      }).then(response => console.log(response))
+    }
   }
 };
 </script>
